d246855f1179f25976ca17974f047c407aa8b35d
imwillson <liwillson1@gmail.com>
2019-01-08 02:46:14 -0500

    add a problem

--- 1.py
+++ 1.py
@@ -1,3 +1,29 @@
-hello hi hey
-
-part 2
+#practice wiht for loop
+class Solution:
+    def numJewelsInStones(self, J, S):
+        """
+        :type J: str
+        :type S: str
+        :rtype: int
+        """
+        
+        sSplit = S.split()
+        
+        jSplit = J.split()
+        
+        count = 0
+        
+        for letter  in sSplit:
+                        
+            if letter in jSplit:
+                count += 1
+                
+                
+        return count
+                
+                
+                
+        
+            
+            
+         

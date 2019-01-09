966d6f59cca71c4a3b123fc8c08579189f586c58
imwillson <liwillson1@gmail.com>
2019-01-09 03:10:58 -0500

    add move twice as fast solution

--- 1.py
+++ 1.py
@@ -20,6 +20,17 @@ class Solution:
                 
                 
         return count
+
+
+# or use a set
+
+def numJewelsInStones(self, J, S):
+        # runtime: 39ms
+        Jset, count = set(J), 0
+        for i in S:
+            if i in Jset:
+                count+=1
+        return count
                 
                 
                 
